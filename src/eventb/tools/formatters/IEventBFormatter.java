aa5cc139ff2f19eed625941d1a88c0d215dbbd0b
stratosphr <stratosphr@gmail.com>
2016-08-05 10:47:28 +0200

    Fixed hierarchy.

--- src/eventb/tools/formatters/IEventBFormatter.java
+++ src/eventb/tools/formatters/IEventBFormatter.java
@@ -2,8 +2,6 @@ package eventb.tools.formatters;
 
 import eventb.Event;
 import eventb.Machine;
-import eventb.expressions.arith.*;
-import eventb.expressions.bool.*;
 import eventb.expressions.sets.CustomSet;
 import eventb.expressions.sets.NamedSet;
 import eventb.expressions.sets.RangeSet;
@@ -13,7 +11,7 @@ import eventb.substitutions.*;
  * Created by gvoiron on 06/07/16.
  * Time : 22:25
  */
-public interface IEventBFormatter {
+public interface IEventBFormatter extends IExpressionFormatter {
 
     String visit(Event event);
 
@@ -26,32 +24,6 @@ public interface IEventBFormatter {
     String visit(Select select);
 
     String visit(IfThenElse ifThenElse);
-
-    String visit(True aTrue);
-
-    String visit(Not not);
-
-    String visit(And and);
-
-    String visit(Equals equals);
-
-    String visit(LowerThan lowerThan);
-
-    String visit(LowerOrEqual lowerOrEqual);
-
-    String visit(GreaterThan greaterThan);
-
-    String visit(GreaterOrEqual greaterOrEqual);
-
-    String visit(Implication implication);
-
-    String visit(Variable variable);
-
-    String visit(Int anInt);
-
-    String visit(Subtraction subtraction);
-
-    String visit(Multiplication multiplication);
 
     String visit(CustomSet customSet);
 
